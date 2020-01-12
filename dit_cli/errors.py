b3c12b28aafaf4334d5d40acd869dadc6e581b16
Isaiah Shiner <shiner.isaiah@gmail.com>
2020-01-12 09:44:57 -0500

    Removed errors.py, didn't work

--- dit_cli/errors.py
+++
@@ -1,9 +0,0 @@
-"""Storage for error strings"""
-
-# pylint: disable=invalid-name
-# pylint: disable=line-too-long
-errors = {
-    "dit": {
-        "doc_type": 'Dit error: file did not begin with "<!DOCTYPE dit xml>".\nFirst 25 characters: {}'.format(dit_string[:25])
-    }
-}
