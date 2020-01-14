cccec08fdf8e664dfcc8473bac049e0e1a714459
Isaiah Shiner <shiner.isaiah@gmail.com>
2020-01-14 11:22:10 -0500

    Moved parser and exceptions to their own files

---
+++ dit_cli/exceptions.py
@@ -0,0 +1,8 @@
+"""All Exceptions used by dit_cli"""
+
+
+class ValidationError(Exception):
+    """Raised when anything goes wrong during validation of a dit"""
+
+# class ParseError(Exception):
+    #"""Raised when there anything goes wrong during paring the file"""
---
+++ dit_cli/parser.py
@@ -0,0 +1,152 @@
+"""Holds the Parser class"""
+
+from typing import List
+
+import re
+
+from dit_cli.exceptions import ValidationError
+
+
+class Parser:
+    """Contains all string functions which interact directly with the dit.
+    Will include abstractions for different parser types in the future.
+    Currently, only XML is valid."""
+
+    def __init__(self, parser_type: str):
+        self.check_type(parser_type)
+        self.containing_tokens = [
+            'header', 'dit', 'objects',
+            'fields', 'object', 'field',
+            'extends', 'extend', 'overrides',
+            'override', 'contains', 'contain'
+        ]
+
+        self.value_tokens = [
+            'name', 'description', 'example',
+            'validator', 'converter', 'variable',
+            'payload'
+        ]
+
+        self.white_space_tokens = [
+            'validator', 'converter', 'payload'
+        ]
+
+    def trim_dit(self, dit: str, replace_str: str) -> str:
+        """Clear value and white space from dit"""
+        return dit.replace(replace_str, '', 1).lstrip()
+
+    def trim_dit_safe(self, dit: str, replace_str: str, tokens: List[str]) -> str:
+        """Clear value from the dit,
+        and clear white space if the
+        current token is not a white space token"""
+        if tokens[-1] in self.white_space_tokens:
+            return dit.replace(replace_str, '', 1)
+        else:
+            return self.trim_dit(dit, replace_str)
+
+    def is_token(self, token: str) -> bool:
+        """Check if a str is on any list of valid tokens"""
+        return token in self.containing_tokens or token in self.value_tokens
+
+    def is_container(self, token: str) -> bool:
+        """Check if str is a containing token"""
+        return token in self.containing_tokens
+
+    def is_value(self, token: str) -> bool:
+        """Check if str is a value token"""
+        return token in self.value_tokens
+
+    def get_xml_tag(self, dit: str) -> str:
+        """Get an XML tag, whether it has a closing '/' or not"""
+        left_brace = dit.find('<')
+
+        if left_brace == -1:
+            raise ValidationError((
+                'Parse Error: No opening brace found.'
+                'Next 30 characters:\n{}'
+            ).format(dit[:30]))
+
+        right_brace = dit.find('>')
+
+        if right_brace == -1:
+            raise ValidationError((
+                'Parse Error: No closing brace found.'
+                'Next 30 characters:\n{}'
+            ).format(dit[:30]))
+
+        if left_brace > right_brace:
+            raise ValidationError((
+                'Parse Error: Missing "<" in "{}"'
+            ).format(dit[:right_brace + 1]))
+
+        tag = dit[left_brace:right_brace + 1]
+
+        # '</description' is 14 chars
+        if len(tag) > 14:
+            raise ValidationError((
+                'Parse Error: Missing ">" in "{}"'
+            ).format(tag))
+
+        return tag
+
+    def get_open(self, dit: str) -> (str, str):
+        """Get the opening token, raise if there isn't one."""
+
+        open_raw = self.get_xml_tag(dit)
+        open_regex = '^<([a-z])+>$'
+
+        if not re.search(open_regex, open_raw):
+            raise ValidationError((
+                'Parse Error: Could not find valid open tag within "{}".'
+            ).format(open_raw))
+
+        open_value = open_raw[1: -1]
+
+        if not self.is_token(open_value):
+            raise ValidationError((
+                'Parse Error: The token "{}" is not a valid token name.'
+            ).format(open_value))
+
+        return (open_raw, open_value)
+
+    def get_close(self, dit: str, open_value: str) -> str:
+        """Get the closing token, raise if there isn't one."""
+        close_raw = self.get_xml_tag(dit)
+        close_regex = '^</' + open_value + '>$'
+
+        if not re.search(close_regex, close_raw):
+            raise ValidationError((
+                'Parse Error: Could not find valid close token within "{}".'
+            ).format(close_raw))
+        return close_raw
+
+    def get_value(self, dit: str, open_value: str) -> str:
+        """Get all text until the next close"""
+        calculated_close = '</{}>'.format(open_value)
+        return dit[:dit.index(calculated_close)]
+
+    def get_open_if_present(self, dit: str) -> bool:
+        """Returns the open, or false if there isn't one.
+        Will never raise a ValidationException."""
+        try:
+            return self.get_open(dit)
+        except ValidationError:
+            return False
+
+    def get_close_if_present(self, dit: str, open_value: str) -> bool:
+        """Returns the close, or false if there isn't one.
+        Will never raise a ValidationException."""
+        try:
+            return self.get_close(dit, open_value)
+        except ValidationError:
+            return False
+
+    def check_type(self, parser_type: str):
+        """Raise an error if it's not 'xml'"""
+
+        if parser_type != 'xml':
+            raise ValidationError((
+                'Feature WIP: "{}" is not currently supported. '
+                'The parser will be eventually be arbitrary, '
+                'but for now, only "xml" is valid'
+            ).format(parser_type))
