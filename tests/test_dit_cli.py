02249b552874dcb1c7e9d1502e5986a2f7d7277e
Isaiah Shiner <shiner.isaiah@gmail.com>
2020-04-07 13:28:45 -0400

    Overhaul of tests and example dits

--- tests/test_dit_cli.py
+++ tests/test_dit_cli.py
@@ -1,75 +1,66 @@
 """Pytest unit tests"""
 
-import os
-import json
-import pytest
-from dit_cli.cli import validate_dit
 
-DIT_DIR = os.path.join(os.path.dirname(__file__), 'dits/')
+import os
+import pytest
+
+from dit_cli.cli import validate_dit
 VALID_STR = 'dit is valid, no errors found'
 
 
-@pytest.mark.long
-def test_classroom():
-    result = validate_dit(get_file('classroom.dit'))
-    assert result == VALID_STR
+@pytest.mark.short
+@pytest.mark.parametrize("file_name,expected", [
+    ('dits/conflicts1.dit', VALID_STR),
+    ('dits/conflicts2.dit', VALID_STR),
+    ('dits/escape1.dit', VALID_STR),
+    ('dits/eval1.dit', VALID_STR),
+    ('dits/inheritance1.dit', VALID_STR),
+    ('dits/list1.dit', VALID_STR),
+    ('dits/misc1.dit', VALID_STR),
+    ('dits/query1.dit', VALID_STR),
+    ('dits/space3.dit', VALID_STR),
+])
+def test_validate(file_name, expected):
+    result = validate_dit(get_file(file_name))
+    assert result == expected
 
 
 @pytest.mark.short
-def test_escape():
-    result = validate_dit(get_file('escape.dit'), 'print(escape1)')
-    assert result == '''"Let's"'''
-
-
-@pytest.mark.short
-def test_extension():
-    result = validate_dit(get_file('extension.dit'))
-    assert result == VALID_STR
-
-
-@pytest.mark.short
-def test_fruits():
-    result = validate_dit(get_file('fruits.dit'), 'fav.value')
-    assert result == '"kiwi"'
-
-
-@pytest.mark.long
-def test_lengths():
-    result = validate_dit(get_file('lengths.dit'))
-    assert result == VALID_STR
+@pytest.mark.parametrize("file_name,expected", [
+    ('fail/assign1.dit', 'AssignError: Assigner "a" expected 1 args, got 2'),
+    ('fail/assign2.dit', 'AssignError: Undefined arg "garbage" for Assigner a'),
+])
+def test_raise(file_name, expected):
+    result = validate_dit(get_file(file_name)).args[0]
+    assert result == expected
 
 
 @pytest.mark.long
-def test_multiple_inheritance():
-    result = validate_dit(get_file('multiple_inheritance.dit'))
-    assert result == VALID_STR
+@pytest.mark.parametrize("file_name,query,expected,", [
+    ('dits/escape1.dit', 'print(e.escape1)', '''"Let's"'''),
+    ('dits/query1.dit', 'print(name)', '"Jane Emily Marie Doe"'),
+    ('dits/query1.dit', 'name',
+     '''{"class":"FullName","print":"Jane Emily Marie Doe","givenName":{"class":"Name","print":"Jane","value":"Jane"},"middleNames":[{"class":"Name","print":"Emily","value":"Emily"},{"class":"Name","print":"Marie","value":"Marie"}],"familyName":{"class":"Name","print":"Doe","value":"Doe"}}'''),
+])
+def test_query(file_name, query, expected):
+    result = validate_dit(get_file(file_name), query_string=query)
+    assert result == expected
 
 
-@pytest.mark.short
-def test_name():
-    result = validate_dit(get_file('name.dit'), 'print(myName)')
-    assert result == '"Hi! My name is Isaiah Leopold Koser Shiner"'
-
-
-@pytest.mark.short
-def test_syntax_enums():
-    result = validate_dit(get_file('syntax_enums.dit'))
-    assert result == VALID_STR
-
-
-@pytest.mark.short
-def test_validator():
-    result = validate_dit(get_file('validator.dit'))
-    assert result == VALID_STR
-
-
-@pytest.mark.short
-def test_json():
-    result = validate_dit(get_file('name.dit'), 'myName')
-    json.loads(result)
+@pytest.mark.long
+@pytest.mark.parametrize("file_name,expected", [
+    ('../examples/fruit.dit', VALID_STR),
+    ('../examples/import.dit', VALID_STR),
+    ('../examples/length.dit', VALID_STR),
+    ('../examples/name.dit', VALID_STR),
+    ('../examples/school.dit', VALID_STR),
+])
+def test_examples(file_name, expected):
+    result = validate_dit(get_file(file_name))
+    assert result == expected
 
 
 def get_file(file_name):
     """Helper to turn a test file name into the file"""
-    with open(os.path.join(DIT_DIR, file_name)) as file_object:
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as file_object:
         return file_object.read()
