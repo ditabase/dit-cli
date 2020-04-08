9a904e8b114460bd71b6e63df131cc194e5d9fe7
Isaiah Shiner <shiner.isaiah@gmail.com>
2020-04-08 13:26:01 -0400

    Changed CodeError test to work with any NodeJS

--- tests/test_dit_cli.py
+++ tests/test_dit_cli.py
@@ -33,24 +33,6 @@ def test_validate(file_name, expected):
     ('fail/assigner1.dit', 'AssignError: Assigner "a" expected 1 args, got 2'),
     ('fail/assigner2.dit', 'AssignError: Undefined arg "garbage" for Assigner a'),
     ('fail/evaler1.dit', 'ValidationError<a>: This is a test error'),
-    ('fail/evaler2.dit', '''CodeError: A Javascript Validator
-Error message follows:
-
-/tmp/dit/A-Validator.js:3
-        const a = 2;
-              ^
-
-SyntaxError: Identifier 'a' has already been declared
-    at Object.<anonymous> (/tmp/dit/A-Validator.js:5:23)
-    at Module._compile (internal/modules/cjs/loader.js:778:30)
-    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)
-    at Module.load (internal/modules/cjs/loader.js:653:32)
-    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)
-    at Function.Module._load (internal/modules/cjs/loader.js:585:3)
-    at Function.Module.runMain (internal/modules/cjs/loader.js:831:12)
-    at startup (internal/bootstrap/node.js:283:19)
-    at bootstrapNodeJSCore (internal/bootstrap/node.js:623:3)
-'''),
     ('fail/namespace-att1.dit', 'VarError: Expected string "A.value", got "B"'),
     ('fail/namespace-att2.dit', 'VarError: Expected class "A", got string'),
     ('fail/namespace-att3.dit', 'VarError: Expected "A", got "C"'),
@@ -69,6 +51,19 @@ SyntaxError: Identifier 'a' has already been declared
 def test_raise(file_name, expected):
     result = validate_dit(get_file(file_name)).args[0]
     assert result == expected
+
+
+@pytest.mark.short
+def test_code_error():
+    result = validate_dit(get_file('fail/evaler2.dit')).args[0]
+    assert result.startswith('''CodeError: A Javascript Validator
+Error message follows:
+
+/tmp/dit/A-Validator.js:3
+        const a = 2;
+              ^
+
+SyntaxError: Identifier 'a' has already been declared''')
 
 
 @pytest.mark.long
